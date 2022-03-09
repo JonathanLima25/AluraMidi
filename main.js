1bae6e21e5e8c29cc624888905059e4844eb38af
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-09 13:48:13 -0300

    captur event

--- main.js
+++ main.js
@@ -6,4 +6,12 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas.forEach(i => i.onclick=function(){
     tocaSom(`#som_${i.classList[1]}`)
+    i.onkeydown = function(event) {
+        if(event.code === 'Enter' || event.code === 'Space'){
+            i.classList.add('ativa');
+        }
+    }
+    i.onkeyup = function() {
+        i.classList.remove('ativa');
+    }
 })
